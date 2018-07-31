aaf24b2e359d613791d339e9ff0340a3d2708225
Hydrothermal <hydrothermal@tuta.io>
2018-07-31 15:01:40 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,143 @@
+var WIDTH = 1498,
+    HEIGHT = 1122,
+    ctx;
+
+var mx, my, locked;
+
+function shade(color, ratio) {   
+    var f=parseInt(color.slice(1),16),t=ratio<0?0:255,p=ratio<0?ratio*-1:ratio,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
+function generateQuest(q) {
+    var obj = q.objectives,
+        giv = $("<div class='circle giv' id='g" + q.id + "' hover='" + q.name.replace(/'/g, "&#39;") + "'></div>");
+
+    if(q.reverse) {
+        giv.addClass("reverse");
+    }
+
+    giv.css({
+        left: q.x,
+        top: q.y,
+        backgroundColor: q.color,
+        border: "2px solid " + shade(q.color, -0.7)
+    }).appendTo("body");
+
+    // visible quest IDs
+    // $("<div class='number'>" + q.id + "</div>").css("color", q.color).appendTo(giv);
+
+    for(var i = 0; i < obj.length; i++) {
+        $("<div class='circle obj o" + q.id + "'></div>").css({
+            left: obj[i][0],
+            top: obj[i][1],
+            backgroundColor: q.color
+        }).appendTo("body");
+    }
+}
+
+$(function() {
+    var hovertag = $("<div id='hovertag' class='tag'><div class='lip lip1'></div><div class='lip lip2'></div><div class='content'></div></div>").appendTo("body"),
+        canvas = $("#canvas")[0];
+
+    ctx = canvas.getContext("2d");
+
+    for(var i = 0; i < quests.length; i++) {
+        generateQuest(quests[i]);
+    }
+    
+    $(".giv:not(#follow)").hover(function() {
+        var id = this.id.substr(1),
+            objectives = $(".o" + id),
+            giver = $(this).offset();
+        
+        $("body, #screen").toggleClass("show");
+        objectives.toggleClass("show");
+        ctx.strokeStyle = shade(quests[id].color, 0.25);
+
+        objectives.each(function() {
+            var offset = $(this).offset();
+
+            ctx.beginPath();
+            ctx.moveTo(giver.left + 7, giver.top + 7);
+            ctx.lineTo(offset.left + 5, offset.top + 5);
+            ctx.stroke();
+        });
+    });
+
+    $(document)
+        .on("mouseenter", "[hover]", function() {
+            var $this = $(this).addClass("hover"),
+                text = "<b>" + $this.attr("hover") + "</b>",
+                offset = $this.offset(),
+                timeout = hovertag.css("opacity") * 100;
+
+            if($this.hasClass("clicked")) {
+                return;
+            }
+
+            setTimeout(function() {
+                // Check if the circle is still hovered in case mouse left before the timeout started
+                if($this.hasClass("hover")) {
+                    hovertag.find(".content").html(text);
+                    hovertag.attr("class", "tag show").css({
+                        top: "initial",
+                        bottom: $(window).innerHeight() - offset.top + 8,
+                        left: offset.left + ($this.outerWidth() / 2) - (hovertag.outerWidth() / 2)
+                    });
+
+                    if($this.hasClass("reverse")) {
+                       hovertag.addClass("reverse").css({
+                            bottom: "initial",
+                            top: offset.top + $this.height() + 2
+                        });
+                    }
+                }
+            }, timeout);
+        })
+        .on("mouseleave", "[hover]", function() {
+            hovertag.removeClass("show");
+            $(this).removeClass("hover");
+            ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        })
+        .on("click", ".giv", function() {
+            $(this).addClass("clicked");
+        })
+
+        // Testing
+        .on("mousemove", "canvas, .giv", function(e) {
+            var x = mx = e.pageX - 10;
+            var y = my = e.pageY - 10;
+
+            $("#follow").css({
+                left: mx,
+                top: my
+            });
+
+            if(locked) {
+                x = locked[0]; y = locked[1];
+            }
+
+            $("#coords").html(`x: ${x}, y: ${y},<br><br>`);
+
+            var objs = $("#xxx .obj:not(#follow)");
+
+            if(objs.length > 0) {
+                $("#coords").append(`[${objs.map(function() {
+                    var o = $(this).offset();
+                    return `[${o.left}, ${o.top}]`;
+                }).toArray().join(", ")}]`);
+            }
+        })
+        .on("click", "canvas", function() {
+            $("#follow").clone().removeAttr("id").appendTo("#xxx");
+            if($("#follow").hasClass("giv")) {
+                locked = [mx, my];
+                $("#follow").removeClass("giv").addClass("obj");
+            }
+        });
+
+    if(document.URL.indexOf("?test") === -1) {
+        $("#coords, #follow").hide();
+    }
+});
---
+++ js/quests.js
@@ -0,0 +1,458 @@
+var quests = [
+    {
+        id: 0,
+        name: "Poor Tenant",
+        x: 779, y: 475,
+        color: "#01a08a",
+        category: "route1",
+        objectives: [[647, 669]]
+    },
+    {
+        id: 1,
+        name: "The Poison Devourer",
+        x: 779, y: 500,
+        color: "#6fd4fd",
+        category: "route1",
+        objectives: [[727, 668]]
+    },
+    {
+        id: 2,
+        name: "The Wayward Wizard",
+        x: 793, y: 500,
+        color: "#ff1a1a",
+        category: "route1",
+        objectives: [[394, 754]]
+    },
+    {
+        id: 3,
+        name: "Bandit Raid",
+        x: 805, y: 500,
+        color: "#ec9e33",
+        category: "route1",
+        objectives: [[656, 582]]
+    },
+    {
+        id: 4,
+        name: "Trouble in the Woods",
+        x: 817, y: 500,
+        color: "#d1ff58",
+        category: "route1",
+        objectives: [[638, 676], [700, 679]]
+    },
+    {
+        id: 5,
+        name: "Charr at the Gate",
+        x: 817, y: 475,
+        color: "#ffa31a",
+        category: "route1",
+        objectives: [[666, 473]]
+    },
+    {
+        id: 6,
+        name: "A Gift for Althea",
+        x: 734, y: 499,
+        color: "#ff3377",
+        category: "route1",
+        objectives: [[642, 651], [665, 561], [666, 673]]
+    },
+    {
+        id: 7,
+        name: "Across the Wall",
+        x: 655, y: 463,
+        color: "#417fc8",
+        category: "route1",
+        objectives: [[612, 304]]
+    },
+    {
+        id: 8,
+        name: "Little Thom's Big Cloak",
+        x: 632, y: 642,
+        color: "#d966ff",
+        category: "route1",
+        objectives: [[552, 769]]
+    },
+    {
+        id: 9,
+        name: "The Hunter's Horn",
+        x: 610, y: 689,
+        color: "#ff6401",
+        category: "route1",
+        objectives: [[659, 998], [731, 943]]
+    },
+    {
+        id: 10,
+        name: "The Egg Hunter",
+        x: 772, y: 641,
+        color: "#1e8018",
+        category: "route1",
+        objectives: [[720, 667], [740, 635], [752, 648]]
+    },
+    {
+        id: 11,
+        name: "The Elementalist Experiment",
+        x: 452, y: 850,
+        color: "#ff1a1a",
+        category: "route1",
+        objectives: []
+    },
+    {
+        id: 12,
+        name: "The Orchard",
+        x: 1043, y: 792,
+        color: "#ff1a1a",
+        category: "route1",
+        objectives: [[1034, 885]]
+    },
+    {
+        id: 13,
+        name: "A Mesmer's Burden",
+        x: 721, y: 498,
+        color: "#c61aff",
+        category: "route1",
+        objectives: [[675, 518]]
+    },
+    {
+        id: 14,
+        name: "A Secondary Profession",
+        x: 766, y: 475,
+        color: "#ff3377",
+        category: "route1",
+        objectives: []
+    },
+    {
+        id: 15,
+        name: "Adventure With An Ally",
+        x: 758, y: 532,
+        color: "#80bfff",
+        category: "route1",
+        objectives: []
+    },
+    {
+        id: 16,
+        name: "A Message From A Friend",
+        x: 793, y: 475,
+        color: "#c61aff",
+        category: "route1",
+        objectives: [[104, 108]]
+    },
+    {
+        id: 17,
+        name: "War Preperations",
+        x: 805, y: 475,
+        color: "#ff6666",
+        category: "route1",
+        objectives: [[766, 500]]
+    },
+    {
+        id: 18,
+        name: "Further Adventures",
+        x: 766, y: 500,
+        color: "#66ff33",
+        category: "route1",
+        objectives: [[638, 676]]
+    },
+    {
+        id: 19,
+        name: "Grawl Invasion",
+        x: 578, y: 483,
+        color: "#ffa31a",
+        category: "route1",
+        objectives: [[504, 547], [513, 546], [505, 543]]
+    },
+    {
+        id: 20,
+        name: "Gwen's Flute",
+        x: 771, y: 546,
+        color: "#ff3377",
+        category: "route1special",
+        objectives: [[742, 590]]
+    },
+    {
+        id: 21,
+        name: "Ranger's Companion",
+        x: 739, y: 760,
+        color: "#80ff00",
+        category: "route1",
+        objectives: [[746, 840]]
+    },
+    {
+        id: 22,
+        name: "The Worm Problem",
+        x: 629, y: 677,
+        color: "#ffa31a",
+        category: "route1",
+        objectives: [[625, 581]]
+    },
+    {
+        id: 23,
+        name: "Protection Prayers",
+        x: 550, y: 481,
+        color: "#80bfff",
+        category: "routemonk",
+        objectives: [[646, 571]]
+    },
+    {
+        id: 24,
+        name: "The Blessings of Balthazar",
+        x: 591, y: 616,
+        color: "#80bfff",
+        category: "routemonk",
+        objectives: [[621, 550]],
+    },
+    {
+        id: 25,
+        name: "Domination Magic",
+        x: 499, y: 783,
+        color: "#c61aff",
+        category: "routemesmer",
+        objectives: [[616, 842]]
+    },
+    {
+        id: 26,
+        name: "The Supremacy of Air",
+        x: 499, y: 783,
+        color: "#ff1a1a",
+        category: "routeelementalist",
+        objectives: [[442, 845]]
+    },
+    {
+        id: 27,
+        name: "Unnatural Growths",
+        x: 647, y: 1001,
+        color: "#01ff8e",
+        category: "routeranger",
+        objectives: [[506, 977]]
+    },
+    {
+        id: 28,
+        name: "A Test of Marksmanship",
+        x: 831, y: 790,
+        color: "#adff2f",
+        category: "routeranger",
+        objectives: []
+    },
+    {
+        id: 29,
+        name: "The Vineyard Problem",
+        x: 363, y: 569,
+        color: "#ffa31a",
+        category: "routewarrior",
+        objectives: [[434, 618]]
+    },
+    {
+        id: 30,
+        name: "Warriors Challenge",
+        x: 374, y: 569,
+        color: "#ffa31a",
+        category: "routewarrior",
+        objectives: [[459, 618]]
+    },
+    {
+        id: 31,
+        name: "The True King",
+        x: 374, y: 569,
+        color: "#ff016f",
+        category: "route2",
+        objectives: [[1038, 748]]
+    },
+    {
+        id: 32,
+        name: "The Rogue's Replacement",
+        x: 760, y: 505,
+        color: "#c61aff",
+        category: "route2special",
+        objectives: []
+    },
+    {
+        id: 33,
+        name: "Opposition To The King",
+        x: 638, y: 676,
+        color: "#ff016f",
+        category: "route2",
+        objectives: [[578, 483], [550, 481], [547, 484]]
+    },
+    {
+        id: 34,
+        name: "The Path To Glory",
+        x: 829, y: 500,
+        color: "#ff016f",
+        category: "route2",
+        objectives: []
+    },
+    {
+        id: 35,
+        name: "Tithe The Ashford Abby",
+        x: 638, y: 676,
+        color: "#c61aff",
+        category: "route2",
+        objectives: [[550, 481]]
+    },
+    {
+        id: 36,
+        name: "Unsettling Rumors",
+        x: 638, y: 676,
+        color: "#ff016f",
+        category: "route2",
+        objectives: [[605, 608], [770, 518]]
+    },
+    {
+        id: 37,
+        name: "A Monks Mission",
+        x: 343, y: 434,
+        color: "#80bfff",
+        category: "route1cata",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 38,
+        name: "Rites Of Rememberance",
+        x: 343, y: 434,
+        color: "#ff3377",
+        category: "route1cata",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 39,
+        name: "The Ncromancer's Novice",
+        x: 343, y: 434,
+        color: "#ff016f",
+        category: "route1cata",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 40,
+        name: "The Accursed Path",
+        x: 343, y: 434,
+        color: "#ff016f",
+        category: "routenecromancer",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 41,
+        name: "The Power Of Blood",
+        x: 547, y: 484,
+        color: "#ff016f",
+        category: "routenecromancercata",
+        objectives: []
+    },
+    {
+        id: 42,
+        name: "Charr In The Catacombs",
+        x: 343, y: 434,
+        color: "#ffa31a",
+        category: "route2cata",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 43,
+        name: "The Prize Moa Bird",
+        x: 629, y: 677,
+        color: "#ff016f",
+        category: "route2cata",
+        objectives: []
+    },
+    {
+        id: 44,
+        name: "Warrior Test",
+        x: 766, y: 500,
+        color: "#ffa31a",
+        category: "routewarrior",
+        objectives: [[743, 579]]
+    },
+    {
+        id: 45,
+        name: "Ranger Test",
+        x: 766, y: 500,
+        color: "#ff016f",
+        category: "routeranger",
+        objectives: [[743, 579]]
+    },
+    {
+        id: 46,
+        name: "Mesmer Test",
+        x: 766, y: 500,
+        color: "#c61aff",
+        category: "routemesmer",
+        objectives: [[735, 552]]
+    },
+    {
+        id: 47,
+        name: "Elementalist Test",
+        x: 766, y: 500,
+        color: "#ff1a1a",
+        category: "routeelementalist",
+        objectives: [[743, 579]]
+    },
+    {
+        id: 48,
+        name: "Monk Test",
+        x: 766, y: 500,
+        color: "#80bfff",
+        category: "routemonk",
+        objectives: [[743, 579]]
+    },
+    {
+        id: 49,
+        name: "Necromancer Test",
+        x: 766, y: 500,
+        color: "#ff016f",
+        category: "routenecromancer",
+        objectives: [[735, 552]]
+    },
+    {
+        id: 50,
+        name: "A New Warrior Trainer",
+        x: 747, y: 526,
+        color: "#ffa31a",
+        category: "route2warrior",
+        objectives: [[578, 483]]
+    },
+    {
+        id: 51,
+        name: "A New Ranger Trainer",
+        x: 747, y: 526,
+        color: "#ff016f",
+        category: "route2ranger",
+        objectives: [[739, 760]]
+    },
+    {
+        id: 52,
+        name: "A New Mesmer Trainer",
+        x: 747, y: 526,
+        color: "#c61aff",
+        category: "route2mesmer",
+        objectives: [[723, 502]]
+    },
+    {
+        id: 53,
+        name: "A New Elementalist Trainer",
+        x: 747, y: 526,
+        color: "#ff1a1a",
+        category: "route2elementalist",
+        objectives: [[452, 850]]
+    },
+    {
+        id: 54,
+        name: "A New Monk Trainer",
+        x: 591, y: 616,
+        color: "#80bfff",
+        category: "route2monk",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 55,
+        name: "A New Necromancer Trainer",
+        x: 747, y: 526,
+        color: "#ff016f",
+        category: "route2necromancercata",
+        objectives: [[134, 85]]
+    },
+    {
+        id: 56,
+        name: "Vanguard Quests Eventually",
+        x: 766, y: 475,
+        color: "#ff3377",
+        category: "routev",
+        objectives: [[1000, 800]]
+    }
+];
